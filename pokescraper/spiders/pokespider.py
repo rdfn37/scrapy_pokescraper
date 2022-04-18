d41659e3c6228a793b10f4afcdf6e3bdc67ac255
rdfn37 <88458654+rdfn37@users.noreply.github.com>
2022-04-18 15:22:21 -0300

    Type, ability and moves scapping added.

--- pokescraper/spiders/pokespider.py
+++ pokescraper/spiders/pokespider.py
@@ -23,21 +23,32 @@ class PokeSpider(scrapy.Spider):
                 team = []
                 pokemon_imgs = leaders.css('tr')[0].xpath('.//td//a//img/@src').getall()
                 pokemon_levels = leaders.css('tr')[2].css('td.level::text').getall()
+
+                pokemon_abilities = leaders.xpath('.//tr[4]//td//a//text()').getall()
+                    # pokemon_abilities.append(pokemon_ability)
                 
                 # pokemon_types = leaders.xpath('.//tr[4]//td//a//img/@src').getall()
 
                 for indexPokemon, pokemons  in enumerate(leaders.css('tr')[1].xpath('.//td')):
                     pokemon_types = []
+                    pokemon_moves = []
+                    # pokemon_abilities = []
 
                     for types in leaders.xpath('.//tr[4]//td'):
                         pokemon_type = types.xpath('.//a//img/@src').getall()
                         pokemon_types.append(pokemon_type)
+                    
+                    for moves in leaders.xpath('.//tr[5]//td'):
+                        pokemon_move = moves.xpath('.//a//text()').getall()
+                        pokemon_moves.append(pokemon_move)
 
                     pokemon = {
                         'name': pokemons.xpath('.//a//text()').get(),
                         'img_url': 'https://www.serebii.net' + pokemon_imgs[indexPokemon - 1],
                         'level': pokemon_levels[indexPokemon - 1],
-                        'type': pokemon_types[indexPokemon - 1]
+                        'type': pokemon_types[indexPokemon - 1],
+                        'ability': pokemon_abilities[indexPokemon - 1],
+                        'moves': pokemon_moves[indexPokemon - 1]
                     }
                     
                     if pokemon['name']:
